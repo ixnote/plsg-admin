9eb22b9c74a5f3becb00c98b1beebaf95b4da219
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2024-07-19 00:07:04 +0100

    resolve conflict

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,15 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import ToastProvider from '@/components/ToastProvider';
 import ReduxProvider from '@/components/providers/redux-provider';
-import "./globals.css";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+import './globals.css';
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "PLSG Dashboard",
-  description: "This is admin dashboard for plateau state",
+  title: 'PLSG Dashboard',
+  description: 'This is admin dashboard for plateau state',
 };
 
 export default function RootLayout({
@@ -20,17 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-<<<<<<< HEAD
-    <html lang='en'>
+    <html className={`${GeistSans.variable} ${GeistMono.variable}`} lang='en'>
       <body className={inter.className}>
         <ReduxProvider>
           <ToastProvider>{children}</ToastProvider>
         </ReduxProvider>
       </body>
-=======
-    <html className={`${GeistSans.variable} ${GeistMono.variable}`} lang="en">
-      <body className={inter.className}>{children}</body>
->>>>>>> c08e9c23421ed6b6c650b447ba70c52028611e8b
     </html>
   );
 }
