0f909438f9bcbfc9de090d480fbdb1a1173c4bd5
Edikan <victrudoh@gmail.com>
2024-07-21 10:36:41 +0100

    UPDATE: Added node version to 'package.json'

--- app/(admin)/components/news/SectionInfoSection.tsx
+++ app/(admin)/components/news/SectionInfoSection.tsx
@@ -1,7 +1,7 @@
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { CldUploadWidget } from 'next-cloudinary';
-import Image from 'next/image';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
 import {
   ALargeSmall,
   FileVideo,
@@ -14,13 +14,13 @@ import {
   Pilcrow,
   Trash2Icon,
   UploadCloud,
-} from 'lucide-react';
-import React, { useEffect, useState } from 'react';
-import { useForm, useFieldArray, Controller } from 'react-hook-form';
-import SectionBtn from './SectionBtn';
-import { useCreateNewsSectionMutation } from '@/redux/services/news/news-api';
-import Loader from '../Loader';
-import { showToast } from '@/lib/showToast';
+} from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
+import SectionBtn from "./SectionBtn";
+import { useCreateNewsSectionMutation } from "@/redux/services/news/news-api";
+import Loader from "../Loader";
+import { showToast } from "@/lib/showToast";
 
 type SectionInfoSectionProps = {
   data: any;
@@ -36,9 +36,9 @@ const SectionInfoSection = ({ data }: SectionInfoSectionProps) => {
     defaultValues: {
       fields: [
         {
-          type: 'paragraph',
-          value: '',
-          position: data.data.newsSections.length + 1,
+          type: "paragraph",
+          value: "",
+          position: data?.data?.newsSections?.length + 1,
         },
       ],
     },
@@ -46,13 +46,13 @@ const SectionInfoSection = ({ data }: SectionInfoSectionProps) => {
 
   const { fields, append, remove } = useFieldArray({
     control,
-    name: 'fields',
+    name: "fields",
   });
 
   const handleAddSection = (type: string) => {
     const section = {
       type: type,
-      value: '',
+      value: "",
       position: data.data.newsSections.length + 1 + fields.length,
     };
     append(section);
@@ -65,24 +65,24 @@ const SectionInfoSection = ({ data }: SectionInfoSectionProps) => {
       position: number;
     }[];
 
-    if (!payload.every((b) => b.value !== '')) {
+    if (!payload.every((b) => b.value !== "")) {
       // showToast('error', <p>All Sections Fields are required</p>);
       return;
     }
 
-    const bullet = payload.filter((b) => b.type === 'bullet');
+    const bullet = payload.filter((b) => b.type === "bullet");
 
     const modifyBullets = bullet?.map((v) => ({
       type: v.type,
-      value: v.value.split(','),
+      value: v.value.split(","),
       position: v.position,
     }));
 
-    const remain = payload.filter((b) => b.type !== 'bullet');
+    const remain = payload.filter((b) => b.type !== "bullet");
 
     const combine = [
       ...remain,
-      ...modifyBullets.map((b) => ({ type: 'bullet', value: b })),
+      ...modifyBullets.map((b) => ({ type: "bullet", value: b })),
     ];
 
     try {
@@ -94,50 +94,50 @@ const SectionInfoSection = ({ data }: SectionInfoSectionProps) => {
         id: data.data.id,
         items: [...combine],
       }).unwrap();
-      showToast('success', <p>{result?.message}</p>);
+      showToast("success", <p>{result?.message}</p>);
     } catch (error: any) {
-      showToast('error', <p>{error?.data?.message}</p>);
+      showToast("error", <p>{error?.data?.message}</p>);
     }
   };
 
   return (
-    <div className='flex flex-col w-1/2 rounded-xl border bg-card text-card-foreground shadow p-6 mb-10 h-fit gap-4'>
-      <div className='flex flex-col gap-1'>
-        <h1 className='font-semibold tracking-tight text-xl'>
+    <div className="flex flex-col w-1/2 rounded-xl border bg-card text-card-foreground shadow p-6 mb-10 h-fit gap-4">
+      <div className="flex flex-col gap-1">
+        <h1 className="font-semibold tracking-tight text-xl">
           Section Information
         </h1>
         <p>Add Sections in order you want to display on the page.</p>
       </div>
-      <div className='flex flex-col w-full py-4 '>
-        <form onSubmit={handleSubmit(onSubmit)} className=' space-y-6'>
+      <div className="flex flex-col w-full py-4 ">
+        <form onSubmit={handleSubmit(onSubmit)} className=" space-y-6">
           {fields.map((field, index) => (
-            <div key={field.id} className='flex w-full gap-3 items-center'>
-              <div className='flex flex-col w-full gap-2'>
-                {fields[index].type === 'paragraph' && (
+            <div key={field.id} className="flex w-full gap-3 items-center">
+              <div className="flex flex-col w-full gap-2">
+                {fields[index].type === "paragraph" && (
                   <Controller
                     name={`fields.${index}.value`}
                     control={control}
                     render={({ field }) => (
                       <Input
                         {...field}
-                        placeholder='eg. Some great information'
+                        placeholder="eg. Some great information"
                       />
                     )}
                   />
                 )}
-                {fields[index].type === 'bullet' && (
+                {fields[index].type === "bullet" && (
                   <Controller
                     name={`fields.${index}.value`}
                     control={control}
                     render={({ field }) => (
                       <Input
                         {...field}
-                        placeholder='eg. imformation,news,education'
+                        placeholder="eg. imformation,news,education"
                       />
                     )}
                   />
                 )}
-                {fields[index].type === 'image' && (
+                {fields[index].type === "image" && (
                   <Controller
                     name={`fields.${index}.value`}
                     control={control}
@@ -147,7 +147,7 @@ const SectionInfoSection = ({ data }: SectionInfoSectionProps) => {
                           field.onChange((result?.info! as any).secure_url); // { public_id, secure_url, etc }
                           widget.close();
                         }}
-                        uploadPreset='mymakaranta_preset'
+                        uploadPreset="mymakaranta_preset"
                       >
                         {({ open }) => {
                           function handleOnClick() {
@@ -157,15 +157,15 @@ const SectionInfoSection = ({ data }: SectionInfoSectionProps) => {
                           return (
                             <div
                               onClick={handleOnClick}
-                              className='flex justify-center h-[250px] border border-dashed cursor-pointer  items-center w-full rounded-md relative  overflow-clip'
+                              className="flex justify-center h-[250px] border border-dashed cursor-pointer  items-center w-full rounded-md relative  overflow-clip"
                             >
-                              {field.value === '' ? (
-                                <div className='flex flex-col justify-center items-center gap-2 '>
+                              {field.value === "" ? (
+                                <div className="flex flex-col justify-center items-center gap-2 ">
                                   <UploadCloud />
                                   <h1>Upload image</h1>
                                 </div>
                               ) : (
-                                <Image src={field.value} alt='images' fill />
+                                <Image src={field.value} alt="images" fill />
                               )}
                             </div>
                           );
@@ -176,65 +176,65 @@ const SectionInfoSection = ({ data }: SectionInfoSectionProps) => {
                 )}
               </div>
               <Button
-                type='button'
-                variant={'destructive'}
+                type="button"
+                variant={"destructive"}
                 onClick={() => remove(index)}
               >
                 <Trash2Icon size={20} />
               </Button>
             </div>
           ))}
-          <div className='flex justify-between items-center p-3 w-full border-2 bg-slate-100 border-dashed rounded-lg'>
+          <div className="flex justify-between items-center p-3 w-full border-2 bg-slate-100 border-dashed rounded-lg">
             <SectionBtn
-              title='Add Paragraph'
+              title="Add Paragraph"
               icon={<Pilcrow />}
               onClick={() => {
-                handleAddSection('paragraph');
+                handleAddSection("paragraph");
               }}
             />
             <SectionBtn
-              title='Add Image'
+              title="Add Image"
               icon={<ImageIcon />}
               onClick={() => {
-                handleAddSection('image');
+                handleAddSection("image");
               }}
             />
             <SectionBtn
-              title='Add Bulletin'
+              title="Add Bulletin"
               icon={<ListOrdered />}
               onClick={() => {
-                handleAddSection('bullet');
+                handleAddSection("bullet");
               }}
             />
             <SectionBtn
-              title='Add Header'
+              title="Add Header"
               icon={<Heading1 />}
               onClick={() => {}}
             />
             <SectionBtn
-              title='Add Highlight'
+              title="Add Highlight"
               icon={<Highlighter />}
               onClick={() => {}}
             />
             {/* <SectionBtn title='Add Video' icon={<FileVideo />} onClick={()=>{}} /> */}
             <SectionBtn
-              title='Add Text'
+              title="Add Text"
               icon={<ALargeSmall />}
               onClick={() => {}}
             />
             <SectionBtn
-              title='Add Sub Heading'
+              title="Add Sub Heading"
               icon={<Heading2 />}
               onClick={() => {}}
             />
             <SectionBtn
-              title='Add Section Heading'
+              title="Add Section Heading"
               icon={<LayoutPanelTop />}
               onClick={() => {}}
             />
           </div>
-          <Button type='submit' className='w-full'>
-            {isLoading ? <Loader /> : 'Create Sectios'}
+          <Button type="submit" className="w-full">
+            {isLoading ? <Loader /> : "Create Section"}
           </Button>
         </form>
       </div>
