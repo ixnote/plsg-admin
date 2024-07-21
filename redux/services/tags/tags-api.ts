6439ea4be123489fb29d1964d886be276c3a493a
Edikan <victrudoh@gmail.com>
2024-07-21 09:26:00 +0100

    UPDATE: Finished 'Add resource' feature

--- redux/services/tags/tags-api.ts
+++ redux/services/tags/tags-api.ts
@@ -1,41 +1,62 @@
-import { getValidAuthTokens } from '@/lib/cookies';
-import { RootState } from '@/redux/store';
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { getValidAuthTokens } from "@/lib/cookies";
+import { RootState } from "@/redux/store";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const tagsApi = createApi({
-  reducerPath: 'tagsApi',
+  reducerPath: "tagsApi",
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_BASE_URL!,
     prepareHeaders: (headers, { getState }) => {
       const { token } = getValidAuthTokens();
       // If we have a token set in state, let's assume that we should be passing it.
       if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
+        headers.set("Authorization", `Bearer ${token}`);
       }
       return headers;
     },
   }),
-  tagTypes: ['Tags'],
+  tagTypes: ["Tags"],
   endpoints: (builder) => ({
     createTags: builder.mutation<any, any>({
       query: (body) => {
         return {
-          url: '/tags/add',
-          method: 'POST',
+          url: "/tags/add",
+          method: "POST",
           body: body,
         };
       },
-      invalidatesTags: ['Tags'],
+      invalidatesTags: ["Tags"],
     }),
 
     getAllTags: builder.query<any, void>({
       query: () => ({
-        url: '/tag/news',
-        method: 'GET',
+        url: "/tag/news",
+        method: "GET",
       }),
-      providesTags: ['Tags'],
+      providesTags: ["Tags"],
+    }),
+
+    getAllTopicTags: builder.query<any, void>({
+      query: () => ({
+        url: "/tag/topics",
+        method: "GET",
+      }),
+      providesTags: ["Tags"],
+    }),
+
+    getAllTypeTags: builder.query<any, void>({
+      query: () => ({
+        url: "/tag/types",
+        method: "GET",
+      }),
+      providesTags: ["Tags"],
     }),
   }),
 });
 
-export const { useCreateTagsMutation, useGetAllTagsQuery } = tagsApi;
+export const {
+  useCreateTagsMutation,
+  useGetAllTagsQuery,
+  useGetAllTopicTagsQuery,
+  useGetAllTypeTagsQuery,
+} = tagsApi;
